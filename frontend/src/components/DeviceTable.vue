a830b4c94dd025a6d221a78f357c684f54e05325
Tallon1 <58697687+Tallon1@users.noreply.github.com>
2025-03-18 15:04:58 +0000

    Updating CSS styling

--- frontend/src/components/DeviceTable.vue
+++ frontend/src/components/DeviceTable.vue
@@ -1,16 +1,16 @@
 <template>
   <div>
-    <h1 class="text-xl font-bold mb-4">Tablet Devices Sorted by OS Version</h1>
+    <h1 class="text-xl font-bold mb-4 text-center pt-4">Tablet Devices Sorted by OS Version</h1>
     <table class="table-auto w-full border-collapse border border-gray-300">
       <thead>
-        <tr>
+        <tr class="px-6 py-4 whitespace-nowrap text-sm text-black-500 bg-gray-500">
           <th class="border px-4 py-2">User Agent</th>
           <th class="border px-4 py-2">Device Type</th>
           <th class="border px-4 py-2">OS Version</th>
         </tr>
       </thead>
       <tbody>
-        <tr v-for="device in sortedDevices" :key="device.id">
+        <tr v-for="device in sortedDevices" :key="device.id" class="bg-white hover:bg-gray-200 transition duration-150 ease-in-out cursor-pointer">
           <td class="border px-4 py-2">{{ device.userAgent }}</td>
           <td class="border px-4 py-2">{{ device.deviceType }}</td>
           <td class="border px-4 py-2">{{ device.osVersion }}</td>
